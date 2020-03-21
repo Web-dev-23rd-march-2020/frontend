cc3864f3cc231865726581c5aaac4537d99877f0
bineet singh chadha <bineetchadha@gmail.com>
2020-03-21 14:32:28 +0000

    first draft

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+app.use(express.static('public'));
 
 app.get("/",function(req,res){
 	res.send("<h1>hello</h1>");
@@ -8,6 +8,13 @@ app.get("/",function(req,res){
 app.get("/home",function(req,res){
 	res.render("home.ejs");
 })
+app.get("/login",function(req,res){
+	res.render("login.ejs");
+})
+app.get("/signup",function(req,res){
+	res.render("signup.ejs");
+})
+
 
 app.listen(3000,function(req,res){
 	console.log("server has started")
